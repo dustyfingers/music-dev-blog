09ebdc837e7f7c54ec31f5d7587cbffaa3a1ef8d
dustyfingers <dustyfingers01257@gmail.com>
2019-07-15 00:11:27 -0600

    worked on rendering posts from db,  orgnaizing sass and styling admin area

--- devmusicblog/settings.py
+++ devmusicblog/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'posts.apps.PostsConfig',
     'authors.apps.AuthorsConfig',
     'pages.apps.PagesConfig',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def posts(request):
-    return render(request, 'posts/index.html')
+    posts = Post.objects.order_by('-publish_date').filter(is_published=True)
+    for post in posts:
+        print(post.text)
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts/posts.html', context)
 
 
 def post(request):
