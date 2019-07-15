905bae651e25e1928ccc27475fb76bd0f7918f1d
dustyfingers <dustyfingers01257@gmail.com>
2019-07-14 21:40:58 -0600

    connected blog to db

--- devmusicblog/settings.py
+++ devmusicblog/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'devmusicblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
